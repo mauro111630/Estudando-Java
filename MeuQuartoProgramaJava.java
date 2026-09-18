a58587a3a2e25940cd16e27d674c31e237238646
mauro111630 <mauro111630@gmail.com>
2026-09-18 13:32:59 -0300

    Melhorias e requisitos feitos, sem IA

--- MeuQuartoProgramaJava.java
+++ MeuQuartoProgramaJava.java
@@ -3,9 +3,9 @@ Requisitos do programa Batalha Naval
 [X] Tabuleiro 5x5 como matriz de char, tudo começa com '_'
 [X] Uma segunda matriz 5x5, escondida, onde ficam 5 navios ('N') em posições aleatórias
 [X] Um método que imprime SÓ o tabuleiro visível (o jogador nunca vê a matriz escondida)
-[ ] Pedir linha e coluna do "tiro" a cada rodada
-[ ] Validar a entrada com try/catch e checar se linha/coluna estão entre 0 e 4
-[ ] Impedir atirar de novo numa posição já atacada
+[X] Pedir linha e coluna do "tiro" a cada rodada
+[X] Validar a entrada com try/catch e checar se linha/coluna estão entre 0 e 4
+[X] Impedir atirar de novo numa posição já atacada
 [ ] Se acertar um navio: marcar 'X' no tabuleiro visível e avisar "Acertou!"
 [ ] Se errar: marcar 'O' no tabuleiro visível e avisar "Água!"
 [ ] Contar quantos navios já afundaram
@@ -31,14 +31,42 @@ void main(){
     {'_', '_', '_', '_', '_'},
     {'_', '_', '_', '_', '_'}
   };
-  aletorizarNavios(naviosTabuleiro);
-  mostrarHUD();
-  mostrarTabuleiro(baseTabuleiro);
 }
 
 void mostrarHUD(){
   IO.println("------------------------------------------------");
   IO.println("                   JOGO NAVAL                   ");
+  IO.println("Tabuleiro 5x5");
+  IO.println("Coordenadas Y e X");
+  IO.println(" Y");
+  IO.println("|0| | | | | |");
+  IO.println("|1| | | | | |");
+  IO.println("|2| | | | | |");
+  IO.println("|3| | | | | |");
+  IO.println("|4| | | | | |");
+  IO.println("| |0|1|2|3|4| X");
+  IO.println("------------------------------------------------");
+}
+
+void jogar(char [][] tabuleiro){
+  //validação de entradas do usuário
+  try{
+    int linha = Integer.parseInt(IO.readln("Digite coodenada Y: "));
+    int coluna = Integer.parseInt(IO.readln("Digite coordenada X: "));
+    if(tabuleiro [linha][coluna] == '_'){
+        tabuleiro [linha][coluna] = 'X'; 
+        IO.println("Tiro dado na água (¬_¬)");
+    }else{
+        if(tabuleiro [linha][coluna] == 'X'){
+          IO.println("Tiro dado da água, dnv (0_0)");
+        }else{
+          IO.println("ACERTOU em cheio!! (#o#)");  // aqui
+        }
+    }
+  }catch(NumberFormatException | ArrayIndexOutOfBoundsException e) {
+    IO.println("Resposta inválida!");
+    jogar(tabuleiro);
+  }   
 }
 
 void aletorizarNavios(char [][] tabuleiro){

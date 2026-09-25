369e8df07e6fd2744f56b80f106fd28a020841dc
mauro111630 <mauro111630@gmail.com>
2026-09-25 15:05:11 -0300

    Implementação da lógica de conferir 8 casas ao redor de uma, me sentir um gênio, até a depressão diminuiu um pouco

--- MeuQuintoProgramaJava.java
+++ MeuQuintoProgramaJava.java
@@ -1,9 +1,9 @@
 /*
 Requisitos do programa - Campo Minado
 [X] Tabuleiro 8x8 como matriz de char, tudo começa oculto (ex: '#')
-[] Uma segunda matriz 8x8, escondida, com 10 minas ('M') em posições aleatórias
-[ ] Um método que, para uma casa sem mina, conta quantas minas existem nas até 8 casas vizinhas (cuidado com os limites do tabuleiro!)
-[ ] Um método que imprime SÓ o tabuleiro visível
+[X] Uma segunda matriz 8x8, escondida, com 10 minas ('M') em posições aleatórias
+[X] Um método que, para uma casa sem mina, conta quantas minas existem nas até 8 casas vizinhas (cuidado com os limites do tabuleiro!)
+[X] Um método que imprime SÓ o tabuleiro visível
 [ ] Pedir linha e coluna da casa a revelar
 [ ] Validar a entrada com try/catch e checar se linha/coluna estão entre 0 e 7
 [ ] Impedir revelar de novo uma casa já revelada
@@ -39,11 +39,15 @@ void main(){
     {'#', '#', '#', '#', '#', '#', '#', '#'}
   };
   aletorizarMinas(tabuleiroInterior);
-  mostrarTabuleiro(tabuleiroInterior);
-  IO.println("-------------------------------------");
-  IO.println("");
-  aletorizarMinas(tabuleiroInterior);
-  mostrarTabuleiro(tabuleiroInterior);
+  mostrarHUD(tabuleiroInterior);
+  IO.print("Coordenada Y:");
+  int coordenadaY = jogar();
+  IO.print("Coordenada X:");
+  int coordenadaX = jogar();
+  int qtdBombasPerto = conferir8CasasEmVolta(tabuleiroInterior, coordenadaY, coordenadaX);
+  IO.print("Bombas perto:" + qtdBombasPerto);
+
+ 
 }
 
 void aletorizarMinas(char [][] tabuleiro){
@@ -75,6 +79,7 @@ void limparTabela(char [][] tabuleiro){
   }
 }
 
+//método para testar o código
 void mostrarTabuleiro(char [][] tabuleiro){
   for(int linha = 0; linha < 8; linha++){
     for(int coluna = 0; coluna < 8; coluna++){
@@ -84,3 +89,139 @@ void mostrarTabuleiro(char [][] tabuleiro){
   }
 }
 
+int conferir8CasasEmVolta(char [][] tabuleiro, int coordenadaY, int coordenadaX){
+  int qtdBombas = 0;
+  // posição fora do tabuleiro, o try/catch ignora e continua
+  /*
+    Fiz algumas contas e descobrir que são um total de 8 possibilidades(8 casas em volta da casa recebida), e alguns de padrões surgem, 
+    e ai concluir as seguinter afirmações:
+    ((1+-Y),(1+-X)) -> mesmo sinal entre grandezas, duas possibilidades
+    ((1+-Y),(1+-X)) -> diferentes sinais entre grandezas, duas possibilidades
+    ((Y),(1+-X)) -> duas possibilidades
+    ((1+-Y),(X)) -> duas possibilidades
+    totalizando as 8 possibilidades, obrigado Deus Jeová pelas minhas faculdades mentais.
+  */
+  try{
+    if(tabuleiro[coordenadaY + 1][coordenadaX + 1] == 'M'){
+      qtdBombas++;
+    }
+  }catch(ArrayIndexOutOfBoundsException e){}
+  
+  try{
+    if(tabuleiro[coordenadaY - 1][coordenadaX - 1] == 'M'){
+      qtdBombas++;
+    }
+  }catch(ArrayIndexOutOfBoundsException e){} 
+  
+  try{
+    if(tabuleiro[coordenadaY + 1][coordenadaX - 1] == 'M'){
+      qtdBombas++;
+    }
+  }catch(ArrayIndexOutOfBoundsException e){} 
+    
+  try{
+    if(tabuleiro[coordenadaY - 1][coordenadaX + 1] == 'M'){
+      qtdBombas++;
+    }
+  }catch(ArrayIndexOutOfBoundsException e){} 
+  
+  try{
+    if(tabuleiro[coordenadaY][coordenadaX + 1] == 'M'){
+      qtdBombas++;
+    }
+  }catch(ArrayIndexOutOfBoundsException e){} 
+    
+  try{
+    if(tabuleiro[coordenadaY][coordenadaX - 1] == 'M'){
+      qtdBombas++;
+    }
+  }catch(ArrayIndexOutOfBoundsException e){} 
+  
+  try{
+    if(tabuleiro[coordenadaY + 1][coordenadaX] == 'M'){
+      qtdBombas++;
+    }
+  }catch(ArrayIndexOutOfBoundsException e){} 
+  
+  try{
+    if(tabuleiro[coordenadaY - 1][coordenadaX] == 'M'){
+      qtdBombas++;
+    }
+  }catch(ArrayIndexOutOfBoundsException e){} 
+  
+  return qtdBombas;
+}
+
+void mostrarHUD(char [][] base){
+  IO.println("------------------------------------------------");
+  IO.println("            JOGO CAMPO MINADO                   ");
+  IO.println("Tabuleiro 5x5");
+  IO.println("Coordenadas Y e X");
+  IO.println(" Y");
+  
+  IO.print("|0|");
+  for(int i = 0; i<8; i++){            //mostrar a tabela de forma mais intuitiva
+    IO.print(base[0][i] +"|");
+  }
+  IO.println("");
+  IO.print("|1|");
+  for(int i = 0; i<8; i++){         
+    IO.print(base[1][i] +"|");
+  }
+  IO.println("");  
+  IO.print("|2|");
+  for(int i = 0; i<8; i++){         
+    IO.print(base[2][i] +"|");
+  }
+  IO.println("");
+  IO.print("|3|");
+  for(int i = 0; i<8; i++){         
+    IO.print(base[3][i] +"|");
+  }
+  IO.println("");  
+  IO.print("|4|");
+  for(int i = 0; i<8; i++){         
+    IO.print(base[4][i] +"|");
+  }
+  IO.println("");  
+  IO.print("|5|");
+  for(int i = 0; i<8; i++){         
+  IO.print(base[5][i] +"|");
+  }
+  IO.println("");
+  IO.print("|6|");
+  for(int i = 0; i<8; i++){         
+  IO.print(base[6][i] +"|");
+  }
+  IO.println("");
+  IO.print("|7|");
+  for(int i = 0; i<8; i++){         
+  IO.print(base[7][i] +"|");
+  }
+  IO.println(""); 
+  
+  
+  IO.println("| |0|1|2|3|4|5|6|7| X");
+  IO.println("------------------------------------------------");
+}
+//entrada de valor do usuário
+int jogar(){
+  //variáveis locais
+  int respostaJogada = 0;      
+  boolean controleJogo = true; 
+  //controle do jogo
+  while(controleJogo == true){
+    boolean statusResposta = true;
+    //validação de entradas do usuário
+    try{
+      respostaJogada = Integer.parseInt(IO.readln(""));
+    }catch(NumberFormatException e) {
+      IO.println("Resposta inválida!");
+      statusResposta = false;
+    }   
+    if(statusResposta == true){  //Validando se o jogador digitou um valor válido
+      controleJogo = false;
+    }
+  }
+  return respostaJogada;
+}
